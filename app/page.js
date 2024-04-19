99046742c5b21a4156fefb068f6ad8f783ebdda5
Delali34 <ernestanyomitse752@gmail.com>
2024-04-19 08:28:24 +0000

    good

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
-import { getPosts } from "@/services/index";
+import { getPosts, getCategories } from "@/services/index";
 import Blogbanner from "@/components/Blogbanner";
 import PostCard from "@/components/PostCard";
 import Navbar from "@/components/Navbar";
@@ -10,21 +9,43 @@ import Recent from "@/components/Recent";
 
 const Page = () => {
   const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await getPosts();
-        setPosts(data);
-        setLoading(false); // Set loading to false after data is fetched
+        const postData = await getPosts();
+        setPosts(postData);
+
+        const categoryData = await getCategories();
+        setCategories(categoryData);
+
+        setLoading(false);
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("Error fetching data:", error);
       }
     }
 
     fetchData();
   }, []);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleAllClick = () => {
+    setSelectedCategory(null);
+  };
+
+  const filteredPosts = selectedCategory
+    ? posts.filter((post) =>
+        post.node.categories.some(
+          (category) => category.slug === selectedCategory
+        )
+      )
+    : posts;
 
   return (
     <div>
@@ -35,28 +56,45 @@ const Page = () => {
       </div>
       <div className="bg-[#f7f1e1]">
         <Recent />
-
-        <div className="max-w-5xl mt-10 mx-auto h-[100%] pb-10 w-full">
-          <div>
-            {/* {posts[0] && <Blogbanner post={posts[0].node} />} */}
-            {/* <Categories /> */}
-            <div className="px-5 mb-8">
-              <div className="lg:mt-20 mt-10">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-                  {loading
-                    ? Array.from({ length: 4 }, (_, index) => (
-                        <div key={index} className="flex">
-                          <div className="animate-pulse w-full bg-gray-100 h-[180px]"></div>
-                        </div>
-                      ))
-                    : posts.map((post, index) => (
-                        <div key={index} className="flex">
-                          <PostCard post={post.node} />
-                        </div>
-                      ))}
-                </div>
-              </div>
-            </div>
+        <div className="max-w-5xl mt-4 mx-auto px-4">
+          <div className="flex flex-wrap justify-center mb-4">
+            <button
+              onClick={handleAllClick}
+              className={`mx-2 my-1 px-3 py-1 rounded-md ${
+                selectedCategory === null
+                  ? "bg-black text-white"
+                  : "border-2 border-black text-black"
+              }`}
+            >
+              All
+            </button>
+            {categories.map((category) => (
+              <button
+                key={category.slug}
+                onClick={() => handleCategoryClick(category.slug)}
+                className={`mx-2 my-1 px-3 py-1 flex-wrap rounded-md ${
+                  selectedCategory === category.slug
+                    ? "bg-black text-white"
+                    : "border-2 border-black text-black"
+                }`}
+              >
+                {category.name}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="max-w-5xl mt-4 mx-auto px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {loading
+              ? Array.from({ length: 4 }, (_, index) => (
+                  <div
+                    key={index}
+                    className="animate-pulse bg-gray-100 h-40"
+                  ></div>
+                ))
+              : filteredPosts.map((post) => (
+                  <PostCard key={post.node.id} post={post.node} />
+                ))}
           </div>
         </div>
       </div>
